cdf9fa5a533926c30922e7dbef13f7b81b0eb50a
yorkmena <mathur.int@gmail.com>
2019-03-08 18:46:01 +0530

    sixth commit

--- searchV2.js
+++ searchV2.js
@@ -3,20 +3,21 @@ import http from "k6/http"
 
 let Protocol = "http://"
 let Host = "internal-digital-movies-app-alb-1839613365.ap-south-1.elb.amazonaws.com"
-let API_city = "/v2/movies/search"
+let API_city = "/v2/movies/search/\?"
 
 export let options = {
     vus: 1,
+
     duration: "10s"
   };
   
 export default function() {
-    //var url = "http://test.loadimpact.com/login";
     
-    var url = Protocol+Host+API_city+"\?city=delhi-ncr";
-    console.log(url);
+    var url = Protocol+Host+API_city+"city=delhi-ncr";
+    //console.log(url);
     //var payload = JSON.stringify({ email: "aaa", password: "bbb" });
     //var params =  { headers: { "Content-Type": "application/json" } }
     //http.post(url, payload, params);
-    http.get(url);
+    var res=http.get(url);
+    console.log(res);
   };
