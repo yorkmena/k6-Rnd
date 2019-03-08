bfdd0f26d8443e8cc768b926f6dea48c820a34e2
yorkmena <mathur.int@gmail.com>
2019-03-08 16:46:31 +0530

    first commit

---
+++ rw.js
@@ -0,0 +1,9 @@
+
+
+Protocol="http";
+Host = "internal-digital-movies-app-alb-1839613365.ap-south-1.elb.amazonaws.com"
+API_city="V2/movies/search"
+   var url = Host+API_city+"?city=delhi-ncr";
+   console.log(url);
+
+  
---
+++ searchV2.js
@@ -0,0 +1,20 @@
+import http from "k6/http"
+
+let Protocol = "http//"
+let Host = "internal-digital-movies-app-alb-1839613365.ap-south-1.elb.amazonaws.com"
+let API_city = "/V2/movies/search"
+
+export let options = {
+    vus: 1,
+    duration: "4s"
+  };
+  
+export default function() {
+    //var url = "http://test.loadimpact.com/login";
+    var url = Protocol+Host+API_city+"?city=delhi-ncr";
+    console.log(url);
+    //var payload = JSON.stringify({ email: "aaa", password: "bbb" });
+    //var params =  { headers: { "Content-Type": "application/json" } }
+    //http.post(url, payload, params);
+    http.get(url);
+  };
